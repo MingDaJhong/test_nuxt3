865e0104dc37d210c19225f8fd1d858e4e8d4483
Darren <iammdchung@gmail.com>
2023-08-04 14:56:56 +0800

    chore: change host

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       script: './.output/server/index.mjs',
       env: {
         NITRO_PORT: 3000,
-        NITRO_HOST: '127.0.0.1'
+        NITRO_HOST: '0.0.0.0'
       }
     }
   ]
